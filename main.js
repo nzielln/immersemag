9f4a0f8fa08e37756a82a6b04dba502605228f74
nzielln <ellah.nzikoba@gmail.com>
2019-05-17 08:21:50 -0400

    second comming

--- main.js
+++ main.js
@@ -1,6 +1,54 @@
 $(function() {
+let $nextbtn1 = $('.svg1');
+let $nextbtn2 = $('.svg2');
+let $nextbtn3 = $('.svg3');
+let $counter1 = 0;
+let $counter2 = 0;
+let $counter3 = 0;
 
-//get window and view poisition --> to trigger animation on when div is in view
+//item 1
+$nextbtn1.click(() => {
+  if ($counter1 == 0) {
+   $('#item1-top').attr('src', 'landingpage/IMG_1112.JPG');
+  $counter1++;
+  console.log($counter1)
+} else if ($counter1 == 1) {
+  $('#item1-top').attr('src', 'landingpage/IMG_1119.JPG');
+ $counter1--;
+ console.log($counter1)
+
+}
+})
+//item2
+$nextbtn2.click(() => {
+  if ($counter2 == 0) {
+   $('#item2-top').attr('src', 'landingpage/IMG_1113.JPG');
+  $counter2++;
+  console.log($counter2)
+} else if ($counter2 == 1) {
+  $('#item2-top').attr('src', 'landingpage/IMG_1115.JPG');
+ $counter2--;
+ console.log($counter2)
+
+}
+})
+//item3
+$nextbtn3.click(() => {
+  if ($counter3 == 0) {
+   $('#item3-top').attr('src', 'landingpage/IMG_1121.JPG');
+  $counter3++;
+  console.log($counter3)
+} else if ($counter3 == 1) {
+  $('#item3-top').attr('src', 'landingpage/IMG_1122.JPG');
+ $counter3--;
+ console.log($counter3)
+
+}
+})
+
+
+
+
 
 
 
